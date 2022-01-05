974739ac6a0a2c79f5fdbd3af1ef3e6b205498e8
tyeom <r.Until@gmail.com>
2022-01-05 09:34:26 +0900

    Add files via upload
    알고리즘 로직 샘플 프로젝트 업로드

---
+++ 알고리즘제작_샘플/Playlogic.cs
@@ -0,0 +1,21 @@
+﻿namespace Reversi_Playlogic
+{
+    using Play;
+    using System;
+    using System.Collections.Generic;
+
+    public class Playlogic : IPlay
+    {
+        public string PlayerNickName => "밍";
+
+        public int Player { get; set; }
+
+        public void Move(out int row, out int col, List<List<int>> reversiBoard)
+        {
+            // 랜덤으로 칸을 선택한다.
+            Random rnd = new Random();
+            col = rnd.Next(0, reversiBoard.Count);
+            row = rnd.Next(0, reversiBoard[0].Count);
+        }
+    }
+}
